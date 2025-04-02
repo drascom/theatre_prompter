0481b82ece7b8486b9bcdaeb2385c4bca0ad7818
drascom <drayhancolak@gmail.com>
2025-04-02 23:39:10 +0100

    .

--- save_script.php
+++ save_script.php
@@ -1,7 +1,7 @@
 <?php
 $data = file_get_contents("php://input");
 if ($data) {
-    file_put_contents("final_script_v2.json", $data);
+    file_put_contents("final_script.json", $data);
     echo "Saved successfully";
 } else {
     http_response_code(400);
